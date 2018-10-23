30ca985c1ddcbec2fb0bef96f7792b61e9b1a0c1
anshulsanan <43185859+anshulsanan@users.noreply.github.com>
2018-10-23 09:59:23 -0400

    Add files via upload

---
+++ Assignment/Ecofind/app/src/main/java/eco/find/Contact.java
@@ -0,0 +1,39 @@
+package eco.find;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:(416)289-5000"));
+                startActivity(x);
+            }
+        });
+
+    }
+    public  void nextActivity(View v)
+    {
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Mail.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
